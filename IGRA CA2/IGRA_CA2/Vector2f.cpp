b4d67c2d278c243833620b9441dbea2fc391f0a3
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-18 22:35:10 +0800

    Add functionality to self-assignment operators
    Added functionality to the overloaded self-assignment operators.

--- IGRA CA2/IGRA_CA2/Vector2f.cpp
+++ IGRA CA2/IGRA_CA2/Vector2f.cpp
@@ -17,16 +17,32 @@ void Vector2f::Set(float x, float y)
 
 Vector2f Vector2f::operator+=(const Vector2f &vector)
 {
+    this->x += vector.x;
+    this->y += vector.y;
+
+    return Vector2f(*this);
 }
 
 Vector2f Vector2f::operator-=(const Vector2f &vector)
 {
+    this->x -= vector.x;
+    this->y -= vector.y;
+
+    return Vector2f(*this);
 }
 
 Vector2f Vector2f::operator*=(float multiplier)
 {
+    this->x *= multiplier;
+    this->y *= multiplier;
+
+    return Vector2f(*this);
 }
 
 Vector2f Vector2f::operator/=(float divisor)
 {
+    this->x /= divisor;
+    this->y /= divisor;
+
+    return Vector2f(*this);
 }
