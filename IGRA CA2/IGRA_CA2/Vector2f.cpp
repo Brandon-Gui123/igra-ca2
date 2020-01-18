06b36527037b900c58fec8b3ea6001e44e1dc671
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-18 19:07:44 +0800

    Create definitions for Vector2f constructors

--- IGRA CA2/IGRA_CA2/Vector2f.cpp
+++ IGRA CA2/IGRA_CA2/Vector2f.cpp
@@ -1,1 +1,13 @@
 #include "Vector2f.h"
+
+Vector2f::Vector2f() : x{values[0]}, y{values[1]}
+{
+    // no need to initialize the values array, because it already is initialized with 0 in all elements
+}
+
+Vector2f::Vector2f(float x, float y) : x{values[0]}, y{values[1]}
+{
+    values[0] = x;
+    values[1] = y;
+}
+
