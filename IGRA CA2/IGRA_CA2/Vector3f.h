fa761e45995cf46264dc1a3c4425eed23acfa0d5
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-18 22:58:16 +0800

    Forward declare overloaded self-assign operators
    Forward declared overloads for the self-assignment operators
    +=, -=, *= and /=

--- IGRA CA2/IGRA_CA2/Vector3f.h
+++ IGRA CA2/IGRA_CA2/Vector3f.h
@@ -16,5 +16,17 @@ public:
 
     // Sets an existing Vector3f with the specified values.
     void Set(float x, float y, float z);
+
+    // Adds a vector to the current vector. This also returns the sum.
+    Vector3f operator+=(const Vector3f &vector);
+    
+    // Subtracts a vector from the current vector. This also returns the difference.
+    Vector3f operator-=(const Vector3f &vector);
+    
+    // Multiplies a vector with a scalar. This also returns the product.
+    Vector3f operator*=(float multiplier);
+    
+    // Divides a vector with a scalar. This also returns the quotient.
+    Vector3f operator/=(float divisor);
 };
 
