f57727b0d70886c9feb9307870325e9bdd84f0b0
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-18 22:51:54 +0800

    Forward declare two constructors
    Forward declared a default constructor (no arguments) and a standard
    constructor (3 arguments).

--- IGRA CA2/IGRA_CA2/Vector3f.h
+++ IGRA CA2/IGRA_CA2/Vector3f.h
@@ -6,5 +6,10 @@ public:
     float y;
     float z;
 
+    // Creates a Vector3f with all of its member fields set to 0.
+    Vector3f();
+
+    // Creates a Vector3f with the specified values.
+    Vector3f(float x, float y, float z);
 };
 
