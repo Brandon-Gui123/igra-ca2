a623b7181423d54c25c158cbad3e412c2a50fd2f
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-18 19:36:06 +0800

    Add summary to operator overloads
    Added a comment above each unary operator and binary operator overloads.

--- IGRA CA2/IGRA_CA2/Vector2f.h
+++ IGRA CA2/IGRA_CA2/Vector2f.h
@@ -21,10 +21,20 @@ public:
     void Set(float x, float y);
 };
 
+// Negates a vector.
 Vector2f operator-(const Vector2f &vector);
 
+// Adds 2 vectors and returns the sum.
 Vector2f operator+(const Vector2f &left, const Vector2f &right);
+
+// Subtracts a vector from another vector and returns the difference.
 Vector2f operator-(const Vector2f &left, const Vector2f &right);
+
+// Multiplies a vector with a scalar and returns the product.
 Vector2f operator*(const Vector2f &vector, float multiplier);
+
+// Multiplies a vector with a scalar and returns the product.
 Vector2f operator*(float multiplier, const Vector2f &vector);       // same as previous overloaded operator*, but the multiplier comes first
+
+// Divides a vector by a scalar and returns the quotient.
 Vector2f operator/(const Vector2f &vector, float divisor);
