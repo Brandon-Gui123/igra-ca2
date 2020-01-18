310e3a62f96fe6545e26d0750f247306e66e1852
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-18 22:44:56 +0800

    Add functionality to overloaded binary operators

--- IGRA CA2/IGRA_CA2/Vector2f.cpp
+++ IGRA CA2/IGRA_CA2/Vector2f.cpp
@@ -54,20 +54,25 @@ Vector2f operator-(const Vector2f &vector)
 
 Vector2f operator+(const Vector2f &left, const Vector2f &right)
 {
+    return Vector2f(left.x + right.x, left.y + right.y);
 }
 
 Vector2f operator-(const Vector2f &left, const Vector2f &right)
 {
+    return Vector2f(left.x - right.x, left.y - right.y);
 }
 
 Vector2f operator*(const Vector2f &vector, float multiplier)
 {
+    return Vector2f(vector.x * multiplier, vector.y * multiplier);
 }
 
 Vector2f operator*(float multiplier, const Vector2f &vector)
 {
+    return Vector2f(vector.x * multiplier, vector.y * multiplier);
 }
 
 Vector2f operator/(const Vector2f &vector, float divisor)
 {
+    return Vector2f(vector.x / divisor, vector.y / divisor);
 }
