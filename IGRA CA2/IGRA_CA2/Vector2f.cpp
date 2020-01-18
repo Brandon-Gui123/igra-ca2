a0cedaf5a7a869c5200a3828bf62ab184754799a
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-18 22:26:38 +0800

    Create definitions of functions in header file
    Created definitions of the functions from the header file
    into the source file

--- IGRA CA2/IGRA_CA2/Vector2f.cpp
+++ IGRA CA2/IGRA_CA2/Vector2f.cpp
@@ -1,1 +1,14 @@
 #include "Vector2f.h"
+
+Vector2f::Vector2f() : x{0}, y{0}
+{}
+
+Vector2f::Vector2f(float x, float y) : x{x}, y{y}
+{}
+
+Vector2f::~Vector2f()
+{}
+
+void Vector2f::Set(float x, float y)
+{
+}
