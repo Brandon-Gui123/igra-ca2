45b7ada5a9f8025d60f02f8e60945c11e864b8ab
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-19 17:33:33 +0800

    Add file for testing Vector3f

---
+++ IGRA CA2/IGRA_Unit_Testing/Vector3f_UnitTesting.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+#include "../IGRA_CA2/Vector3f.h"   // the main class to test
+#include "../IGRA_CA2/Vector2f.h"   // to test casting from Vector3f to Vector2f
+
+namespace IGRAUnitTesting
+{
+}
