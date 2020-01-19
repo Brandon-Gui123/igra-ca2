bc1c4fc4dc166f2adafcefda4667544928a1b76e
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-19 17:33:53 +0800

    Add test cases for Vector3f constructors

--- IGRA CA2/IGRA_Unit_Testing/Vector3f_UnitTesting.cpp
+++ IGRA CA2/IGRA_Unit_Testing/Vector3f_UnitTesting.cpp
@@ -8,4 +8,27 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace IGRAUnitTesting
 {
+    TEST_CLASS(Vector3f_Constructors_Test)
+    {
+        TEST_METHOD(Vector3f_Default_Constructor)
+        {
+            Vector3f vector{};
+
+            // vector should be (0.0f, 0.0f, 0.0f)
+            Assert::AreEqual(0.0f, vector.x, 1e-8f);
+            Assert::AreEqual(0.0f, vector.y, 1e-8f);
+            Assert::AreEqual(0.0f, vector.z, 1e-8f);
+        }
+
+        TEST_METHOD(Vector3f_XYZ_Constructor)
+        {
+            Vector3f vector{1.0f, 3.0f, 5.0f};
+
+            // vector should be (1.0f, 3.0f, 5.0f)
+            Assert::AreEqual(1.0f, vector.x, 1e-8f);
+            Assert::AreEqual(3.0f, vector.y, 1e-8f);
+            Assert::AreEqual(5.0f, vector.z, 1e-8f);
+        }
+    };
+
 }
