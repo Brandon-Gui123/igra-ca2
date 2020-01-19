a9ca0e5b2493d724e749c295cf5e4682b9852a08
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-19 17:19:50 +0800

    Add test cases for constructor of Vector2f

--- IGRA CA2/IGRA_Unit_Testing/Vector2f_UnitTesting.cpp
+++ IGRA CA2/IGRA_Unit_Testing/Vector2f_UnitTesting.cpp
@@ -3,6 +3,30 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+#include "../IGRA_CA2/Vector2f.h"	// the main class to test
+#include "../IGRA_CA2/Vector3f.h"	// to test casting from Vector2f to Vector3f
+
 namespace IGRAUnitTesting
 {
+	TEST_CLASS(Vector2f_Constructors_Test)
+	{
+		TEST_METHOD(Vector2f_Default_Constructor)
+		{
+			Vector2f vector{};
+			
+			// vector should be all 0
+			Assert::AreEqual(0.0f, vector.x, 1e-8f);
+			Assert::AreEqual(0.0f, vector.y, 1e-8f);
+		}
+
+		TEST_METHOD(Vector2f_XY_Constructor)
+		{
+			Vector2f vector{10.0f, 25.0f};
+
+			// vector should be (10.0f, 25.0f)
+			Assert::AreEqual(10.0f, vector.x, 1e-8f);
+			Assert::AreEqual(25.0f, vector.y, 1e-8f);
+		}
+	};
+
 }
