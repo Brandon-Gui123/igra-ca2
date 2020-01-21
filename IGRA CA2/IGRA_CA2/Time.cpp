dce33e123499ed002a483949a1447aac8551a86d
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-21 17:57:12 +0800

    Add private and public variables to

--- IGRA CA2/IGRA_CA2/Time.cpp
+++ IGRA CA2/IGRA_CA2/Time.cpp
@@ -1,3 +1,6 @@
 #include "Time.h"
 
+Timer Time::internalTimer{};
+double Time::m_deltaTime{};
 
+const double &Time::deltaTime{Time::m_deltaTime};
--- IGRA CA2/IGRA_CA2/Time.h
+++ IGRA CA2/IGRA_CA2/Time.h
@@ -1,7 +1,14 @@
 #pragma once
+#include "Timer.h"
+
 class Time
 {
 private:
+	static Timer internalTimer;
+	static double m_deltaTime;
+
 public:
+	static const double &deltaTime;
+
 };
 
