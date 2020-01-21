957d1d785e20241e075c8e267007284cd57a6cd5
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-21 16:52:29 +0800

    Reformat Timer
    Reformatted Timer to use uniform initialization, static_cast for
    converting integers to doubles, reinterpret_cast to convert from
    __int64* to LARGE_INTEGER* and distributed stuff over new lines.

--- IGRA CA2/IGRA_CA2/Timer.cpp
+++ IGRA CA2/IGRA_CA2/Timer.cpp
@@ -4,43 +4,48 @@
 
 void Timer::StartTimer()
 {
-    __int64 currentTimeInCounts;
+    __int64 currentTimeInCounts{};
 
     // countsPerSecond depends on your PC
-    if (!QueryPerformanceFrequency(
-        (LARGE_INTEGER *) &countsPerSecond))
+    if (!QueryPerformanceFrequency(reinterpret_cast<LARGE_INTEGER*>(&countsPerSecond)))
     {
         MessageBox(NULL, L"QueryPerformanceFrequencyFailed.", L"ERROR", MB_OK | MB_ICONINFORMATION);
         return;
     }
-    QueryPerformanceCounter((LARGE_INTEGER *) &currentTimeInCounts);
+
+    QueryPerformanceCounter(reinterpret_cast<LARGE_INTEGER*>(&currentTimeInCounts));
+
     startTimeInCounts = currentTimeInCounts;
     lastTimeInCounts = currentTimeInCounts;
 }
 
 double Timer::GetTimePassedSinceStart()
 {
-    __int64 currentTimeInCounts;
-    double timePassedSeconds;
+    __int64 currentTimeInCounts{};
+    double timePassedSeconds{};
+
     // Calculate time passed in seconds since timer was started
-    QueryPerformanceCounter((LARGE_INTEGER *) &currentTimeInCounts);
-    timePassedSeconds = (currentTimeInCounts - startTimeInCounts) /
-        (double) countsPerSecond;
+    QueryPerformanceCounter(reinterpret_cast<LARGE_INTEGER*>(&currentTimeInCounts));
+
+    timePassedSeconds = (currentTimeInCounts - startTimeInCounts) / static_cast<double>(countsPerSecond);
 
     return timePassedSeconds;
 }
 
 double Timer::GetTimePassedSinceLastTime()
 {
-    __int64 currentTimeInCounts, timePassedSinceLastTimeInCounts;
+    __int64 currentTimeInCounts{};
+    __int64 timePassedSinceLastTimeInCounts{};
+
     // Calculate time passed in seconds since last call to
     // GetTimePassedSinceLastTime
-    QueryPerformanceCounter((LARGE_INTEGER *) &currentTimeInCounts);
-    timePassedSinceLastTimeInCounts =
-        currentTimeInCounts - lastTimeInCounts;
-    double timePassedSinceLastTimeInSeconds =
-        (currentTimeInCounts - lastTimeInCounts) /
-        (double) countsPerSecond;
+    QueryPerformanceCounter(reinterpret_cast<LARGE_INTEGER*>(&currentTimeInCounts));
+
+    timePassedSinceLastTimeInCounts = currentTimeInCounts - lastTimeInCounts;
+
+    double timePassedSinceLastTimeInSeconds{(currentTimeInCounts - lastTimeInCounts) / static_cast<double>(countsPerSecond)};
+
     lastTimeInCounts = currentTimeInCounts;
+
     return timePassedSinceLastTimeInSeconds;
 }
