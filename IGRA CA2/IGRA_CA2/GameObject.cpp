2bcc1f2e196b58a002a33dd7c31d49450de16f6c
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-24 18:22:48 +0800

    Add GameObject destructor implementation
    This will free up memory occupied by the components, as well as clear
    the vector of components so that it is empty and does not contain
    null pointers.

--- IGRA CA2/IGRA_CA2/GameObject.cpp
+++ IGRA CA2/IGRA_CA2/GameObject.cpp
@@ -6,7 +6,19 @@ GameObject::GameObject()
 {}
 
 GameObject::~GameObject()
-{}
+{
+    for (Component *&comp : components)
+    {
+        // free memory occupied by the components
+        // then set it to a null pointer to prevent undefined behaviour due to
+        // dereferencing freed memory
+        delete comp;
+        comp = nullptr;
+    }
+
+    // clear the vector, which contains null pointers
+    components.clear();
+}
 
 int GameObject::GetComponentCount()
 {
