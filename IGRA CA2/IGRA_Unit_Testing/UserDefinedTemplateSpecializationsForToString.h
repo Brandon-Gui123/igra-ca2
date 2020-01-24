8749904f7e76136101e8cc54f27c32aa778202a8
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-25 01:25:54 +0800

    Add template specialization for FooComponent
    Added ToString template specialization for FooComponent

--- IGRA CA2/IGRA_Unit_Testing/UserDefinedTemplateSpecializationsForToString.h
+++ IGRA CA2/IGRA_Unit_Testing/UserDefinedTemplateSpecializationsForToString.h
@@ -3,6 +3,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+#include "FooComponent.h"   // for class FooComponent
 #include "TestComponent.h"  // for class TestComponent
 
 namespace Microsoft
@@ -12,6 +13,7 @@ namespace Microsoft
         namespace CppUnitTestFramework
         {
             template<> static std::wstring ToString<TestComponent>  (const TestComponent &t)    { return L"TestComponent"; }
+            template<> static std::wstring ToString<FooComponent>   (const FooComponent &t)     { return L"FooComponent"; }
         }
     }
 }
