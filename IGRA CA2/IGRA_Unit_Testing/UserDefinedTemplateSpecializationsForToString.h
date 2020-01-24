2f83026c6d84771b3a44ab6f00dbacf9f4302f43
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-25 01:16:46 +0800

    Add template specialization for TestComponent
    Added a ToString template specialization for the class TestComponent

--- IGRA CA2/IGRA_Unit_Testing/UserDefinedTemplateSpecializationsForToString.h
+++ IGRA CA2/IGRA_Unit_Testing/UserDefinedTemplateSpecializationsForToString.h
@@ -3,13 +3,15 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+#include "TestComponent.h"  // for class TestComponent
+
 namespace Microsoft
 {
     namespace VisualStudio
     {
         namespace CppUnitTestFramework
         {
-
+            template<> static std::wstring ToString<TestComponent>  (const TestComponent &t)    { return L"TestComponent"; }
         }
     }
 }
