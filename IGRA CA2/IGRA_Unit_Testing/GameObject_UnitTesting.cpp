82a7e9fd5deefbd3c32f855275214ce86875066c
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-24 18:23:04 +0800

    Add test case for GameObject destructor

--- IGRA CA2/IGRA_Unit_Testing/GameObject_UnitTesting.cpp
+++ IGRA CA2/IGRA_Unit_Testing/GameObject_UnitTesting.cpp
@@ -19,6 +19,37 @@ namespace IGRAUnitTesting
         }
     };
 
+    TEST_CLASS(GameObject_Destructor)
+    {
+        TEST_METHOD(Default_GameObject_Destructor)
+        {
+            // declare a new kind of component
+            class TestComponent : public Component
+            {
+            public:
+                TestComponent(GameObject &go) : Component{go} {};
+                ~TestComponent() {};
+            };
+
+            GameObject go{};
+
+            // let's add a bunch of components!
+            for (int i{0}; i < 10; ++i)
+            {
+                go.AddComponent<TestComponent>();
+            }
+
+            // the GameObject should now have 10 components
+            Assert::AreEqual(10, go.GetComponentCount());
+
+            // call the destructor to remove the components
+            go.~GameObject();
+
+            // there should be 0 components in the GameObject
+            Assert::AreEqual(0, go.GetComponentCount());
+        }
+    };
+
     TEST_CLASS(GameObject_AddComponent_Test)
     {
         TEST_METHOD(Adding_A_Component)
