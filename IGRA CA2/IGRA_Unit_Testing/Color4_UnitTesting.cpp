a90131ab8225efade40b98f1b98c69be5a670381
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-29 11:23:30 +0800

    Add test case for Color4 rgba constructor
    Added test case for Color4f(T r, T g, T b, T a) constructor

--- IGRA CA2/IGRA_Unit_Testing/Color4_UnitTesting.cpp
+++ IGRA CA2/IGRA_Unit_Testing/Color4_UnitTesting.cpp
@@ -19,5 +19,16 @@ namespace IGRAUnitTesting
             Assert::AreEqual(0.f, color.blue, 1e-8f);
             Assert::AreEqual(0.f, color.alpha, 1e-8f);
         }
+
+        TEST_METHOD(Color4_RGBA_Constructor)
+        {
+            Color4<float> color{0.f, 58.f, 12.f, 0.1f};
+
+            // values should be 0, 58, 12, 0.1
+            Assert::AreEqual(0.f, color.red, 1e-8f);
+            Assert::AreEqual(58.f, color.green, 1e-8f);
+            Assert::AreEqual(12.f, color.blue, 1e-8f);
+            Assert::AreEqual(0.1f, color.alpha, 1e-8f);
+        }
     };
 }
