7025f4b98e914ff4d0cfdfb72d46a82c3f3c6b83
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-29 21:19:41 +0800

    Add #include for inline files for color classes
    The #include directive is placed where the template implementations
    originally were so that existing functionality can continue as usual,
    while still being able to keep header files short.

--- IGRA CA2/IGRA_CA2/Color4.h
+++ IGRA CA2/IGRA_CA2/Color4.h
@@ -21,13 +21,7 @@ public:
     Color4(float red, float green, float blue, float alpha);
 };
 
-template<typename T>
-inline Color4<T>::Color4() : red{0}, green{0}, blue{0}, alpha{0}
-{}
-
-template<typename T>
-inline Color4<T>::Color4(float red, float green, float blue, float alpha) : red{red}, green{green}, blue{blue}, alpha{alpha}
-{}
+#include "Color4.inl"
 
 // define most common types of Color4
 
