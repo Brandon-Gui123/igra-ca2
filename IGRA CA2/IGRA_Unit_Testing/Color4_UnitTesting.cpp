82fa4f28ee46e4d9f94e1de9819906c9df94ac4e
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-29 11:20:24 +0800

    Add file to unit test project to test Color4

---
+++ IGRA CA2/IGRA_Unit_Testing/Color4_UnitTesting.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+#include "../IGRA_CA2/Color4.h"     // the main class to test, Color4
+
+namespace IGRAUnitTesting
+{
+}
