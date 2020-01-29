d79d6e2a4c5cda9c941686424950bb61dec80734
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-29 11:00:38 +0800

    Add test case for Color3 rgb constructor
    Added a test case for Color3(T r, T g, T b) constructor

--- IGRA CA2/IGRA_Unit_Testing/Color3_UnitTesting.cpp
+++ IGRA CA2/IGRA_Unit_Testing/Color3_UnitTesting.cpp
@@ -13,12 +13,21 @@ namespace IGRAUnitTesting
         TEST_METHOD(Color3_Default_Constructor)
         {
             Color3<float> color{};
-            
+
             // all values should be 0
             Assert::AreEqual(0.f, color.red, 1e-8f);
             Assert::AreEqual(0.f, color.green, 1e-8f);
             Assert::AreEqual(0.f, color.blue, 1e-8f);
         }
 
+        TEST_METHOD(Color3_RGB_Constructor)
+        {
+            Color3<float> color{0.f, 23.f, 87.f};
+
+            // values should be 0, 23 and 87
+            Assert::AreEqual(0.f, color.red, 1e-8f);
+            Assert::AreEqual(23.f, color.green, 1e-8f);
+            Assert::AreEqual(87.f, color.blue, 1e-8f);
+        }
     };
 }
