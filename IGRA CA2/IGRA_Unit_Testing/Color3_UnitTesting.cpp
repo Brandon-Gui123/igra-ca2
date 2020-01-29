f4a69b3c9104ccd076bbf2a32e73f8ec1e007ed0
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-29 10:59:11 +0800

    Add file in unit test project to test Color3

---
+++ IGRA CA2/IGRA_Unit_Testing/Color3_UnitTesting.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+#include "../IGRA_CA2/Color3.h"     // the main class we want to test, Color3
+#include "../IGRA_CA2/Color4.h"     // for class Color4, to test implicit conversion from Color3 to Color4
+
+namespace IGRAUnitTesting
+{
+}
