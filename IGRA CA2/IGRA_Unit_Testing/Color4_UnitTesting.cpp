edf8cee3ba97b627eade17f14b1fa05c2ed8fc71
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-29 11:20:34 +0800

    Add test case for Color4 default constructor

--- IGRA CA2/IGRA_Unit_Testing/Color4_UnitTesting.cpp
+++ IGRA CA2/IGRA_Unit_Testing/Color4_UnitTesting.cpp
@@ -7,4 +7,17 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace IGRAUnitTesting
 {
+    TEST_CLASS(Color4_Constructors)
+    {
+        TEST_METHOD(Color4_Default_Constructor)
+        {
+            Color4<float> color{};
+
+            // values should be 0, 0, 0, 0
+            Assert::AreEqual(0.f, color.red, 1e-8f);
+            Assert::AreEqual(0.f, color.green, 1e-8f);
+            Assert::AreEqual(0.f, color.blue, 1e-8f);
+            Assert::AreEqual(0.f, color.alpha, 1e-8f);
+        }
+    };
 }
