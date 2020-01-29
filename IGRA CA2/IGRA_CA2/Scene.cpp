fbdc199664cd7c362cb7d356909288dacc8f75c3
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-29 14:44:33 +0800

    Add destructor functionality for Scene

--- IGRA CA2/IGRA_CA2/Scene.cpp
+++ IGRA CA2/IGRA_CA2/Scene.cpp
@@ -12,6 +12,16 @@ Scene::Scene()
 
 Scene::~Scene()
 {
+	for (GameObject *&gameObjectPtr : gameObjects)
+	{
+		// deallocate memory occupied by the object pointed to by the pointer
+		// then set the pointer to null pointer to prevent dangling pointers
+		delete gameObjectPtr;
+		gameObjectPtr = nullptr;
+	}
+
+	// clear the vector of null pointers
+	gameObjects.clear();
 }
 
 void Scene::Start()
