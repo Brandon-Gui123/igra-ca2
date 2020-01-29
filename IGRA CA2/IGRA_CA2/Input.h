58427e3bf9ddea8ab1382391382c9b74cc4c38b6
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-29 08:46:55 +0800

    Add method documentation to Input header file

--- IGRA CA2/IGRA_CA2/Input.h
+++ IGRA CA2/IGRA_CA2/Input.h
@@ -8,6 +8,8 @@
 #include <unordered_map>        // for std::unordered_map, similar to a C# dictionary
 #include <vector>               // for std::vector
 
+// A class that interfaces into your device's input system and allows easy access
+// to the status of keys, such as whether a certain is pressed down.
 class Input
 {
 private:
@@ -19,14 +21,28 @@ private:
         bool isUp{false};
     };
 
+    // Sends the key down event to the mapped input keys, affecting the state of the key that is pressed down.
     static void SendKeyDown(const WPARAM &wParam);
+
+    // Sends the key up event to the mapped input keys, affecting the state of the key that is lifted up.
     static void SendKeyUp(const WPARAM &wParam);
+
+    // Resets the statuses of the keys that have been altered.
+    // Only resets statuses that should only last for the current frame.
     static void ResetKeyDownUp();
 
 public:
 
+    // Returns true if the key identified by the given key code is held down.
+    // Otherwise, it returns false.
     static bool GetKey(KeyCode keyCode);
+
+    // Returns true if the key identified by the given key code is held down on this current frame.
+    // Otherwise, it returns false.
     static bool GetKeyDown(KeyCode keyCode);
+
+    // Returns true if the key identified by the given key code is lifted up on this current frame.
+    // Otherwise, it returns false.
     static bool GetKeyUp(KeyCode keyCode);
 
     Input();
