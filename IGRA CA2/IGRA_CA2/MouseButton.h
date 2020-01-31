7bf5fbedf1adec50e73162358d0708731c8da360
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-01-31 11:05:39 +0800

    Add enum class MouseButton

--- IGRA CA2/IGRA_CA2/MouseButton.h
+++ IGRA CA2/IGRA_CA2/MouseButton.h
@@ -1,1 +1,10 @@
 #pragma once
+
+// An enumeration mapped to the buttons on a mouse.
+// This enumeration only have mappings for the left, middle and right mouse buttons.
+enum class MouseButton
+{
+    Left,
+    Middle,
+    Right
+};
