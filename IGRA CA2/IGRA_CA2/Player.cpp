375707ba72b0fcd47d3d8230d4cf37cd18f931cb
Ryan Tan <tanryan777@gmail.com>
2020-02-03 13:18:47 +0800

    Add new Player Movement

--- IGRA CA2/IGRA_CA2/Player.cpp
+++ IGRA CA2/IGRA_CA2/Player.cpp
@@ -8,6 +8,8 @@
 
 Player::Player(GameObject &go) : Component(go)
 {
+	isJumping = false;
+	jumpTimer = 0;
 }
 
 Player::~Player()
@@ -18,9 +20,54 @@ void Player::Start()
 {
 }
 
+void Player::JumpStart(bool left) {
+	if (!isJumping) {
+		PrevPos = gameObject.position;
+		if (left) {
+			direction = { *new Vector3f(0, 0, 1) };
+			gameObject.rotation = { *new Vector3f(0, 0, 0) };
+		}
+		else {
+			direction = { *new Vector3f(1, 0, 0) };
+			gameObject.rotation = { *new Vector3f(0, 90, 0) };
+		}
+		isJumping = true;
+	}
+}
+
+void Player::Jump() {
+	if (jumpTimer < 1) {
+		jumpTimer += Time::GetDeltaTime() * 3;
+		float y = -4 * std::pow(jumpTimer, 2) + 4 * jumpTimer;
+		float m = 4 * std::pow((jumpTimer - 0.5), 3) + 0.5;
+		Vector3f mv = m * direction;
+		Vector3f delta(0, y, 0);
+		delta += mv;
+		gameObject.position = PrevPos + delta;
+	}
+	else {
+		isJumping = false;
+		jumpTimer = 0;
+		gameObject.position = PrevPos + direction;
+		gameObject.position.y = 0;
+	}
+}
+
 void Player::Update()
 {
-	Vector3f delta(Input::GetAxis(Input::x), 0, Input::GetAxis(Input::y));
+	if (isJumping) {
+		Jump();
+	}
+
+	if (Input::GetKeyDown(KeyCode::A)) {
+		JumpStart(true);
+	}
+
+	if (Input::GetKeyDown(KeyCode::D)) {
+		JumpStart(false);
+	}
+
+	/*Vector3f delta(Input::GetAxis(Input::x), 0, Input::GetAxis(Input::y));
 	Vector3f newPos = delta.GetNormalized() * speed * Time::GetDeltaTime() + gameObject.position;
-	gameObject.position = newPos;
+	gameObject.position = newPos;*/
 }
--- IGRA CA2/IGRA_CA2/Player.h
+++ IGRA CA2/IGRA_CA2/Player.h
@@ -1,9 +1,18 @@
 #pragma once
 
 #include "Component.h"
+#include "Vector3f.h"
 
 class Player : public Component
 {
+private:
+	bool isJumping;
+	float jumpTimer;
+	void JumpStart(bool left);
+	void Jump();
+	Vector3f PrevPos;
+	Vector3f direction;
+
 public:
 	void Update() override;
 	void Start() override;
