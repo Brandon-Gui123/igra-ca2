f97334f21a26ebf6752cbfcab23e98638cd1999d
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-03 08:38:08 +0800

    Add constructor functionality
    The constructor calculates and assigns a unique colour value for the
    mesh and adds the constructed instance into the vector.

--- IGRA CA2/IGRA_CA2/PickableMesh.cpp
+++ IGRA CA2/IGRA_CA2/PickableMesh.cpp
@@ -1,11 +1,24 @@
 #include "PickableMesh.h"
 
+#include "Color4.h"     // for Color4ub type alias in Color4
+
+#include "framework.h"  // to support the Windows stuff inside the GL header file
+#include <gl/GL.h>      // for GLubyte
+
 #include <vector>       // for std::vector class
 
 std::vector<PickableMesh*> PickableMesh::pickableMeshes{};
 
 PickableMesh::PickableMesh(GameObject &gameObject) : Mesh{}, gameObject{gameObject}
 {
+    GLubyte vectorSize{static_cast<GLubyte>(pickableMeshes.size())};
+
+    // TODO overkill but maybe make use of all 3 variables for 16,777,216 possibilities
+    // stick with 256 possibilities using just the red value in the color
+
+    pickingColor = Color4ub{static_cast<GLubyte>(vectorSize), static_cast<GLubyte>(0), static_cast<GLubyte>(0), static_cast<GLubyte>(1)};
+
+    pickableMeshes.push_back(this);
 }
 
 PickableMesh::~PickableMesh()
