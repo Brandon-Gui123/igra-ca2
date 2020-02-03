47e9cc24a95d102b4ddb8f02864b64d69f2def09
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-03 08:30:27 +0800

    Add vector to hold all pickable meshes
    These vectors are required as we will be drawing these meshes to the
    back buffer.

--- IGRA CA2/IGRA_CA2/PickableMesh.cpp
+++ IGRA CA2/IGRA_CA2/PickableMesh.cpp
@@ -1,2 +1,6 @@
 #include "PickableMesh.h"
 
+#include <vector>       // for std::vector class
+
+std::vector<PickableMesh*> PickableMesh::pickableMeshes{};
+
--- IGRA CA2/IGRA_CA2/PickableMesh.h
+++ IGRA CA2/IGRA_CA2/PickableMesh.h
@@ -2,6 +2,12 @@
 
 #include "Mesh.h"       // for Mesh class
 
+#include <vector>       // for std::vector class
+
 class PickableMesh : public Mesh
 {
+private:
+
+    static std::vector<PickableMesh*> pickableMeshes;
+
 };
