032418bcefc362399f066bdd6a911dc155730414
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-04 14:48:23 +0800

    Add materials to the player mesh

--- IGRA CA2/IGRA_CA2/PlayerMesh.h
+++ IGRA CA2/IGRA_CA2/PlayerMesh.h
@@ -1,7 +1,25 @@
 #pragma once
+
+#include "Color4.h"		// for Color4f, a type alias of Color4 using GLfloat
+#include "Material.h"	// for Material class
 #include "Mesh.h"
+
 class PlayerMesh : public Mesh
 {
+private:
+
+	// duck's body (yellow)
+	Material bodyMaterial{Color4f{1.f, 1.f, 0.f, 1.f}, Color4f{1.f, 1.f, 0.f, 1.f}, Color4f{1.f, 1.f, 1.f, 1.f}, 32};
+
+	// duck's bill (orange)
+	Material billMaterial{Color4f{1.f, 0.5f, 0.f, 1.f}, Color4f{1.f, 0.5f, 0.f, 1.f}, Color4f{1.f, 1.f, 1.f, 1.f}, 32};
+
+	// duck's legs (orange)
+	Material legsMaterial{Color4f{1.f, 0.5f, 0.f, 1.f}, Color4f{1.f, 0.5f, 0.f, 1.f}, Color4f{1.f, 1.f, 1.f, 1.f}, 32};
+
+	// duck's eye
+	Material eyeMaterial{Color4f{0.f, 0.f, 0.f, 1.f}, Color4f{0.f, 0.f, 0.f, 1.f}, Color4f{1.f, 1.f, 1.f, 1.f}, 32};
+
 public:		
 	void DrawShape() override;
 	PlayerMesh();
