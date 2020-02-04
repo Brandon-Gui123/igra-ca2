f234a1c49fe914f470e79c5ecfe5843a013f469e
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-05 00:28:42 +0800

    Add newlines and indentation

--- IGRA CA2/IGRA_CA2/Mesh.cpp
+++ IGRA CA2/IGRA_CA2/Mesh.cpp
@@ -88,34 +88,42 @@ void Mesh::Draw(const Vector3f &pos, const Vector3f &rot, const Vector3f &sca)
 void Mesh::DrawShape() {
 	GLfloat matcolour[] = { 1, 1, 1, 1 };
 	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, matcolour);
-	glEnable(GL_CULL_FACE);
-	glFrontFace(GL_CW); // Front face is clockwise
-	glPolygonMode(GL_FRONT, GL_FILL);	
-	glEnable(GL_TEXTURE_2D);
-	//glColor3f(1, 1, 1);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
-		64, 64, 0, GL_RGB, GL_UNSIGNED_BYTE, Program::placeholderTexture);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-	glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T, GL_CLAMP);
-	glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	// The index in cubeIndices array
-	// points to next vertex to draw.
-	int index = 0;
-	// Draw the cube quad by quad
-	for (int qd = 0; qd < 6; qd++) {
-		glBegin(GL_POLYGON);
-		glNormal3f(cubeNormals[qd][0], cubeNormals[qd][1], cubeNormals[qd][2]);
-		for (int v = 0; v < 4; v++) {// Four vertices for one quad
-			glTexCoord2f(cubeTextureMap[v][0], cubeTextureMap[v][1]);
-			glVertex3f(cubeVertices[cubeIndices[index]][0],
-				cubeVertices[cubeIndices[index]][1],
-				cubeVertices[cubeIndices[index]][2]);
-			index++; // Move to next vertex in quad
-		}
 
-		glEnd();
+	glEnable(GL_CULL_FACE);
+	{
+		glFrontFace(GL_CW); // Front face is clockwise
+		glPolygonMode(GL_FRONT, GL_FILL);	
+
+		glEnable(GL_TEXTURE_2D);
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+				64, 64, 0, GL_RGB, GL_UNSIGNED_BYTE, Program::placeholderTexture);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+			glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T, GL_CLAMP);
+			glTexParameterf(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+			glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+			// The index in cubeIndices array
+			// points to next vertex to draw.
+			int index = 0;
+
+			// Draw the cube quad by quad
+			for (int qd = 0; qd < 6; qd++) {
+				glBegin(GL_POLYGON);
+				{
+					glNormal3f(cubeNormals[qd][0], cubeNormals[qd][1], cubeNormals[qd][2]);
+					for (int v = 0; v < 4; v++) {// Four vertices for one quad
+						glTexCoord2f(cubeTextureMap[v][0], cubeTextureMap[v][1]);
+						glVertex3f(cubeVertices[cubeIndices[index]][0],
+							cubeVertices[cubeIndices[index]][1],
+							cubeVertices[cubeIndices[index]][2]);
+						index++; // Move to next vertex in quad
+					}
+				}
+				glEnd();
+			}
+		}
+		glDisable(GL_TEXTURE_2D);
 	}
-	glDisable(GL_TEXTURE_2D);
 	glDisable(GL_CULL_FACE);
 }
