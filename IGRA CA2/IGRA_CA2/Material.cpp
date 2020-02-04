7a776481524834b1adb4b94fa908f12f4ada7905
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-04 14:17:10 +0800

    Add #includes in header and source files
    Added the #include directives for the header and source files of the
    Material class.

--- IGRA CA2/IGRA_CA2/Material.cpp
+++ IGRA CA2/IGRA_CA2/Material.cpp
@@ -1,2 +1,7 @@
 #include "Material.h"
 
+#include "Color4.h"     // for Color4f, a type alias of the class Color4 which uses GLfloats
+
+#include "framework.h"  // for Window stuff inside the OpenGL library
+#include <gl/GL.h>      // for OpenGL 32-bit library
+
--- IGRA CA2/IGRA_CA2/Material.h
+++ IGRA CA2/IGRA_CA2/Material.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "Color4.h"     // for Color4f, a type alias of the class Color4 which uses GLfloats
+
+#include "framework.h"  // for Window stuff inside the OpenGL library
+#include <gl/GL.h>      // for OpenGL 32-bit library
+
 class Material
 {
 };
