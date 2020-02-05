144fe0c10e70406c683e88a79cd9d360c0fd63b9
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-05 08:12:44 +0800

    Update GetComponent to search in all 3 vectors
    Since we've added 3 new vectors, it will be required that we search
    through all of them, because if we only search the main Components
    vector, we will not be able to find the component we just added in
    during the Update loop.

--- IGRA CA2/IGRA_CA2/GameObject.h
+++ IGRA CA2/IGRA_CA2/GameObject.h
@@ -125,7 +125,38 @@ inline T& GameObject::AddComponent()
 template<typename T>
 inline T* GameObject::GetComponent()
 {
+	// search in the standard components vector first
 	for (Component *&component : components)
+	{
+		// attempt to downcast from Component to type T (casting to a more specific type)
+		// if the downcast is successful, we will get the component as type T
+		// if the downcast fails, we will get a null pointer to type T
+		T* instance{dynamic_cast<T*>(component)};
+
+		if (instance != nullptr)
+		{
+			// we already found the component, so return it
+			return instance;
+		}
+	}
+
+	// then search in the components that are about to be Start-ed
+	for (Component *&component : componentsToStart)
+	{
+		// attempt to downcast from Component to type T (casting to a more specific type)
+		// if the downcast is successful, we will get the component as type T
+		// if the downcast fails, we will get a null pointer to type T
+		T* instance{dynamic_cast<T*>(component)};
+
+		if (instance != nullptr)
+		{
+			// we already found the component, so return it
+			return instance;
+		}
+	}
+
+	// and finally, the newly-added components
+	for (Component *&component : newlyAddedComponents)
 	{
 		// attempt to downcast from Component to type T (casting to a more specific type)
 		// if the downcast is successful, we will get the component as type T
