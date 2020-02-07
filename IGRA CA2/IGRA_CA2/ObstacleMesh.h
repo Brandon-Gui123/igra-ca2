e768011a716cd6ad5dfd1ad265b7aa6ebc1fdb1a
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-07 21:45:55 +0800

    Add ObstacleMesh class

---
+++ IGRA CA2/IGRA_CA2/ObstacleMesh.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "GameObject.h"     // for GameObject class
+#include "PickableMesh.h"   // to inherit from base class PickableMesh
+
+class ObstacleMesh : public PickableMesh
+{
+};
+
