720e406c1d77cc9b5aba16260e3118256fcf93f8
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-07 21:59:54 +0800

    Add private quadric variable for GLUquadrics

--- IGRA CA2/IGRA_CA2/ObstacleMesh.h
+++ IGRA CA2/IGRA_CA2/ObstacleMesh.h
@@ -3,7 +3,15 @@
 #include "GameObject.h"     // for GameObject class
 #include "PickableMesh.h"   // to inherit from base class PickableMesh
 
+#include "framework.h"      // to define the Windows stuff inside the OpenGL library
+#include <gl/GL.h>          // for the OpenGL 32-bit library
+#include <gl/GLU.h>         // for the OpenGL Utilities 32-bit library
+
 class ObstacleMesh : public PickableMesh
 {
+private:
+
+    GLUquadric *quadric;
+
 };
 
