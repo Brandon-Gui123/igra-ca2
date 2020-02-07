46f4f22e28a648f834b4f1592fcbdf81bc9dd1d1
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-07 23:03:55 +0800

    Add constructor for Obstacle

--- IGRA CA2/IGRA_CA2/Obstacle.cpp
+++ IGRA CA2/IGRA_CA2/Obstacle.cpp
@@ -1,1 +1,6 @@
 #include "Obstacle.h"
+
+#include "GameObject.h" // for class GameObject
+
+Obstacle::Obstacle(GameObject &gameObject) : Component{gameObject}
+{}
--- IGRA CA2/IGRA_CA2/Obstacle.h
+++ IGRA CA2/IGRA_CA2/Obstacle.h
@@ -1,7 +1,10 @@
 #pragma once
 
-#include "Component.h"
+#include "Component.h"  // for base class Component
+#include "GameObject.h" // for class GameObject
 
 class Obstacle : public Component
 {
+public:
+    Obstacle(GameObject &gameObject);
 };
