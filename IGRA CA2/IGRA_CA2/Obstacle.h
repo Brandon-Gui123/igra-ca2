36c01310e4057325eaa8ab20c5134fa31ea743d6
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-08 13:30:51 +0800

    Give obstacle a pointer to the lily it is on

--- IGRA CA2/IGRA_CA2/Obstacle.h
+++ IGRA CA2/IGRA_CA2/Obstacle.h
@@ -2,12 +2,15 @@
 
 #include "Component.h"  // for base class Component
 #include "GameObject.h" // for class GameObject
+#include "Lily.h"       // for Lily component
 
 class Obstacle : public Component
 {
 public:
     Obstacle(GameObject &gameObject);
     ~Obstacle();
+
+    Lily *lily; // The lily that this obstacle is on.
 
     // Called when the mesh of the GameObject is selected.
 	void OnGameObjectSelected() override;
