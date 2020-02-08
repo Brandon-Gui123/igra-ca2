f5310f6a43ba7e4e13a03aa6a6af08fd9705938c
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-08 14:43:24 +0800

    Add credits callback function for the dialog

--- IGRA CA2/IGRA_CA2/OpenGLApplication.cpp
+++ IGRA CA2/IGRA_CA2/OpenGLApplication.cpp
@@ -58,6 +58,7 @@ ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    Credits(HWND, UINT, WPARAM, LPARAM);
 void ReSizeGLScene(GLsizei width, GLsizei height);
 int InitOpenGL();
 void DrawGLScene();
@@ -293,6 +294,25 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
             return (INT_PTR)TRUE;
         }
         break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+INT_PTR CALLBACK Credits(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+        case WM_INITDIALOG:
+            return (INT_PTR)TRUE;
+
+        case WM_COMMAND:
+            if (LOWORD(wParam) == IDCLOSE || LOWORD(wParam) == IDCANCEL)
+            {
+                EndDialog(hDlg, LOWORD(wParam));
+                return (INT_PTR)TRUE;
+            }
+            break;
     }
     return (INT_PTR)FALSE;
 }
