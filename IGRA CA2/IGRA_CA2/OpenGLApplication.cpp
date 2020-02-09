6a1ef2f55bc18efb3fd3acefcf56a1fe2e1fd58b
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2020-02-09 12:26:03 +0800

    Add resuming of game

--- IGRA CA2/IGRA_CA2/OpenGLApplication.cpp
+++ IGRA CA2/IGRA_CA2/OpenGLApplication.cpp
@@ -214,6 +214,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 Time::SetTimeScale(0.0);
                 break;
 
+            case ID_GAME_RESUME:
+                Time::SetTimeScale(1.0);
+                break;
+
             default:
                 return DefWindowProc(hWnd, message, wParam, lParam);
             }
