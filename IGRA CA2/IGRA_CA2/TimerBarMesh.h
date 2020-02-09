06f5836d8ea1e5611e694065df58de5dc89a06f2
ryanntannn <tanryan777@gmail.com>
2020-02-09 18:22:03 +0800

    Add DrawShape

--- IGRA CA2/IGRA_CA2/TimerBarMesh.h
+++ IGRA CA2/IGRA_CA2/TimerBarMesh.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include "Mesh.h";
+#include "Color4.h";
+#include "Material.h"
 
 class TimerBarMesh : public Mesh
 {
+private:
+	Material TimerBarMaterial{ Color4f{1.f, 0.f, 0.f, 1.f}, Color4f{1.f, 0.f, 0.f, 1.f}, Color4f{1.f, 1.f, 1.f, 1.f}, 32 };
+
 public:
+	void DrawShape() override;
 	TimerBarMesh();
 	~TimerBarMesh();
 };
