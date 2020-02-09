654cee34dc3c9a89d7f34c28d19ce5c193cd15b1
ryanntannn <tanryan777@gmail.com>
2020-02-09 18:15:22 +0800

    Add TimerBar Mesh

---
+++ IGRA CA2/IGRA_CA2/TimerBarMesh.cpp
@@ -0,0 +1,12 @@
+#include "TimerBarMesh.h"
+
+
+
+TimerBarMesh::TimerBarMesh()
+{
+}
+
+
+TimerBarMesh::~TimerBarMesh()
+{
+}
---
+++ IGRA CA2/IGRA_CA2/TimerBarMesh.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Mesh.h";
+
+class TimerBarMesh : public Mesh
+{
+public:
+	TimerBarMesh();
+	~TimerBarMesh();
+};
+
